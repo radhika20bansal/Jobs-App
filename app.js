0b74e5ce8a5080ca0196f3b05e5e8e17fd5b0864
Radhika <radhikajanuary20@gmail.com>
2024-03-19 17:34:38 +0530

    security and rate limit package included

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 const app = express();
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 
-const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const authMiddleware = require('./middleware/authentication');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const authMiddleware = require("./middleware/authentication");
 
 app.use(express.json());
 // extra packages
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP to 100 requests per windowMs
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', authMiddleware, jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
